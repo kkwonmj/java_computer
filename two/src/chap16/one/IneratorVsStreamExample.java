38c0df4092f0b203f8ab071d7cba9d8fe8f7ed77
504 <504@DESKTOP-B0KVD9C>
2021-07-29 18:15:33 +0900

    chap15 + 16 ing

---
+++ two/src/chap16/one/IneratorVsStreamExample.java
@@ -0,0 +1,26 @@
+package chap16.one;
+
+import java.util.*;
+import java.util.stream.Stream;
+
+public class IneratorVsStreamExample {
+
+	public static void main(String[] args) {
+		List<String> list = Arrays.asList("홍길동", "신용권", "김자바");
+		
+		//Iterator 이용
+		Iterator<String> iterator = list.iterator();
+		while(iterator.hasNext()) {
+			String name = iterator.next();
+			System.out.println(name);
+		}
+		
+		System.out.println();
+		
+		//Stream 이용
+		Stream<String> stream = list.stream();
+		stream.forEach(name -> System.out.println(name));
+
+	}
+
+}
---
+++ two/src/chap16/two/four/FromDirectoryExample.java
@@ -0,0 +1,17 @@
+package chap16.two.four;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+public class FromDirectoryExample {
+
+	public static void main(String[] args) throws IOException {
+		Path path = Paths.get("D:/hello/source.txt");
+		Stream<Path> stream = Files.list(path);
+		stream.forEach(p -> System.out.println(p.getFileName()));
+	}
+
+}
